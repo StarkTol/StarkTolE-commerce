32a14ae41160c892858eff96e56a2516d156a798
StarkTol <starktol@outlook.com>
2025-08-03 01:39:44 -0700

    Fix Product.ts - add missing mongoose import and export statements

--- models/Product.ts
+++ models/Product.ts
@@ -1,6 +1,12 @@
+import mongoose from "mongoose";
+
 const ProductSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
   price: Number,
   imageUrl: { type: String, required: true },
 });
+
+const Product = mongoose.models.Product || mongoose.model("Product", ProductSchema);
+
+export default Product;
